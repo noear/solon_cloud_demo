02ff2135fdcbf18a79b005fd92a79ec7b3ef70ba
noear <noear@live.cn>
2021-04-26 16:23:04 +0800

    1.3.27

---
+++ cloud-breaker/demo10.solon-cloud-guava/src/main/java/demo10/Demo10App.java
@@ -0,0 +1,12 @@
+package demo10;
+
+import org.noear.solon.Solon;
+
+/**
+ * @author noear 2021/4/26 created
+ */
+public class Demo10App {
+    public static void main(String[] args){
+        Solon.start(Demo10App.class, args);
+    }
+}
---
+++ cloud-breaker/demo11.solon-cloud-sentinel/src/main/java/demo11/Demo11App.java
@@ -0,0 +1,12 @@
+package demo11;
+
+import org.noear.solon.Solon;
+
+/**
+ * @author noear 2021/4/26 created
+ */
+public class Demo11App {
+    public static void main(String[] args){
+        Solon.start(Demo11App.class, args);
+    }
+}
