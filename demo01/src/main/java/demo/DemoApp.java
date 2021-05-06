aa4970c1e4f40f1b34302771418697f9d537d185
noear <noear@live.cn>
2021-05-06 10:13:19 +0800

    1.3.32

--- demo01/src/main/java/demo/DemoApp.java
+++ demo01/src/main/java/demo/DemoApp.java
@@ -1,12 +1,7 @@
 package demo;
 
 import org.noear.solon.Solon;
-import org.noear.solon.Utils;
 import org.noear.solon.cloud.CloudClient;
-import org.noear.solon.cloud.model.Event;
-import org.noear.solon.core.handle.Context;
-import org.noear.solon.core.handle.Filter;
-import org.noear.solon.core.handle.FilterChain;
 import org.slf4j.MDC;
 
 /**
@@ -16,11 +11,10 @@ public class DemoApp {
     public static void main(String[] args) {
         Solon.start(DemoApp.class, args, app -> {
             app.get("/", c -> c.output("Hello world!"));
-        }).filter((ctx, chain) -> {
+        }).before("**", c -> {
+            //将TraceId同步到slf4j
             String traceId = CloudClient.trace().getTraceId();
             MDC.put(CloudClient.trace().HEADER_TRACE_ID_NAME(), traceId);
         });
-
-
     }
 }
