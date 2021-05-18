8b7e0d4fbf318154e77cb493517f714f517a761b
noear <noear@live.cn>
2021-05-18 22:48:22 +0800

    1.4.1

--- cloud-config-discovery-event-log/demo22.solon-cloud-water/demo22-server/src/main/java/demo/controller/HelloServiceRemoteImp.java
+++ cloud-config-discovery-event-log/demo22.solon-cloud-water/demo22-server/src/main/java/demo/controller/HelloServiceRemoteImp.java
@@ -1,15 +1,15 @@
 package demo.controller;
 
 import demo.protocol.HelloService;
-import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Mapping;
+import org.noear.solon.annotation.Remoting;
 
 /**
  * rpc 服务端(solon rpc 基于 uri 进行资源管理；可通过 uri 做多版本实现方式)
  * @author noear 2021/1/8 created
  */
 @Mapping("/rpc/")
-@Component(remoting = true)
+@Remoting
 public class HelloServiceRemoteImp implements HelloService {
     @Override
     public String hello() {
