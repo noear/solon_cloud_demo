fe269eed9b8395471fee81472559a4057ee2d466
noear <noear@live.cn>
2021-05-26 12:00:52 +0800

    1.4.5

--- cloud-job/demo.solon-cloud-xxl-job/src/main/java/demo/DemoJobApp.java
+++ cloud-job/demo.solon-cloud-xxl-job/src/main/java/demo/DemoJobApp.java
@@ -1,12 +1,13 @@
 package demo;
 
 import org.noear.solon.Solon;
+import org.noear.solon.cloud.CloudClient;
 
 /**
  * @author noear 2021/5/25 created
  */
 public class DemoJobApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Solon.start(DemoJobApp.class, args);
     }
 }
