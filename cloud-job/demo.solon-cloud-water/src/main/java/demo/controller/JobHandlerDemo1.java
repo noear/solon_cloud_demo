2cb22a91153c6b49cf4ea0782c4311626921b257
noear <noear@live.cn>
2021-06-05 13:45:25 +0800

    1.4.11

--- cloud-job/demo.solon-cloud-water/src/main/java/demo/controller/JobHandlerDemo1.java
+++ cloud-job/demo.solon-cloud-water/src/main/java/demo/controller/JobHandlerDemo1.java
@@ -1,8 +1,8 @@
 package demo.controller;
 
+import org.noear.solon.cloud.CloudJobHandler;
 import org.noear.solon.cloud.annotation.CloudJob;
 import org.noear.solon.core.handle.Context;
-import org.noear.solon.core.handle.Handler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
  * @author noear 2021/5/25 created
  */
 @CloudJob("JobHandlerDemo1")
-public class JobHandlerDemo1 implements Handler {
+public class JobHandlerDemo1 implements CloudJobHandler {
     static final Logger log = LoggerFactory.getLogger(JobBeanDemo2.class);
 
     @Override
