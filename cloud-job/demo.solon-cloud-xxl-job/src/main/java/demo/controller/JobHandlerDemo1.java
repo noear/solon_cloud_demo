da630d63075fd22a926c6d476f167548602b7f0e
noear <noear@live.cn>
2021-06-10 22:07:04 +0800

    1.4.13

--- cloud-job/demo.solon-cloud-xxl-job/src/main/java/demo/controller/JobHandlerDemo1.java
+++ cloud-job/demo.solon-cloud-xxl-job/src/main/java/demo/controller/JobHandlerDemo1.java
@@ -1,5 +1,6 @@
 package demo.controller;
 
+import org.noear.solon.cloud.CloudJobHandler;
 import org.noear.solon.cloud.annotation.CloudJob;
 import org.noear.solon.core.handle.Context;
 import org.noear.solon.core.handle.Handler;
@@ -8,7 +9,7 @@ import org.noear.solon.core.handle.Handler;
  * @author noear 2021/5/25 created
  */
 @CloudJob("JobHandlerDemo1")
-public class JobHandlerDemo1 implements Handler {
+public class JobHandlerDemo1 implements CloudJobHandler {
     @Override
     public void handle(Context ctx) throws Throwable {
         System.out.println("JobHandlerDemo1");
