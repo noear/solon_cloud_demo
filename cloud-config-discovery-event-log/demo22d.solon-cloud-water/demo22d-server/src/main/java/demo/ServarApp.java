38f169a1c12a669f99b0f8f44b65ef2a51eeed77
noear <noear@live.cn>
2023-01-31 15:38:31 +0800

    2.0.0

--- cloud-config-discovery-event-log/demo22d.solon-cloud-water/demo22d-server/src/main/java/demo/ServarApp.java
+++ cloud-config-discovery-event-log/demo22d.solon-cloud-water/demo22d-server/src/main/java/demo/ServarApp.java
@@ -9,7 +9,7 @@ public class ServarApp {
     public static void main(String[] args) {
         Solon.start(ServarApp.class, args, app -> {
             app.enableSocketD(true);
-            app.enableSafeStop(app.cfg().isFilesMode() == false);
+            app.cfg().enableSafeStop(app.cfg().isFilesMode() == false);
         });
     }
 }
